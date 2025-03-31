bb99c8b567f5899b8235875e39a8dc2f4a709fba
Jason Cox <coxj@vt.edu>
2025-03-31 15:06:27 -0400

    comments and base code for Robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -5,15 +5,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.RobotModel.Robot;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp", group= "Example")
-public class TeleOp extends LinearOpMode {
-
-
+public class TeleOp extends LinearOpMode
+{
     Robot robot;
 
+    /**
+     * Imagine a world where this is all we need to have in this class~
+     * @throws InterruptedException
+     */
     @Override
     public void runOpMode()
             throws InterruptedException
     {
         robot = new Robot(hardwareMap);
+
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            robot.update(gamepad1, gamepad2);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/Robot.java
@@ -1,14 +1,26 @@
 package org.firstinspires.ftc.teamcode.RobotModel;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.RobotModel.DriveTrain.DriveTrain;
+
+import java.lang.reflect.Type;
 
 public class Robot
 {
     DriveTrain driveTrain;
     public Robot(HardwareMap hardwareMap)
     {
+    }
 
+    /**
+     * update the robot state!  Pass along the gamepads to the different modules.
+     * @param gamepad1 driver gamepad
+     * @param gamepad2 mech gamepad
+     */
+    public void update(Gamepad gamepad1, Gamepad gamepad2)
+    {
+        driveTrain.drive(gamepad1);
     }
 }
