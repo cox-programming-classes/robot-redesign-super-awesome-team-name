b553efa181f8dda1f34cdbc3e20b9bfb49f8f060
Eleanor Chen <eleanor.chen@winsor.edu>
2025-04-01 18:12:50 -0400

    idk if i did it right or not but im guessing no

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ If you are an Android Studio programmer, there are several ways to download this
 * If you are a git user, you can clone the most current version of the repository:
 
 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;git clone https://github.com/FIRST-Tech-Challenge/FtcRobotController.git</p>
-
+[teamcode](TeamCode/src/main/java/org/firstinspires/ftc/teamcode)
 * Or, if you prefer, you can use the "Download Zip" button available through the main repository page.  Downloading the project as a .ZIP file will keep the size of the download manageable.
 
 * You can also download the project folder (as a .zip or .tar.gz archive file) from the Downloads subsection of the [Releases](https://github.com/FIRST-Tech-Challenge/FtcRobotController/releases) page for this repository.
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/DriveTrain/MecanumDrive.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.RobotModel.DriveTrain;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MecanumDrive extends DriveTrain
+{
+    // these can be declared Final because once they are initialized they should not be changed.
+    private final DcMotor LB;
+    private final DcMotor LF;
+    private final DcMotor RB;
+    private final DcMotor RF;
+
+    public MecanumDrive(HardwareMap hardwareMap)
+    {
+        LB = hardwareMap.get(DcMotor.class, "LBMotor");
+        LF = hardwareMap.get(DcMotor.class, "LFMotor");
+        RB = hardwareMap.get(DcMotor.class, "RBMotor");
+        RF = hardwareMap.get(DcMotor.class, "<RFMotor>");
+    }
+
+    @Override
+    public void drive(Gamepad gamepad)
+    {
+        double vertical;
+        double horizontal;
+        double turn;
+
+        vertical = gamepad.left_stick_y;
+        horizontal = gamepad.left_stick_x;
+        turn = gamepad.right_stick_x;
+
+        RF.setPower(turn + (- vertical + horizontal));
+        RB.setPower(turn + (- vertical - horizontal));
+        LF.setPower(turn + (- vertical - horizontal));
+        LB.setPower(turn + (- vertical + horizontal));
+    }
+}
