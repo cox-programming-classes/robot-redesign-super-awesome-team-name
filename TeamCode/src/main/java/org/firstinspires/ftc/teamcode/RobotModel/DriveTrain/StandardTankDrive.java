0cd485bf738f5c50a8482499fe2d2aeadb576dad
Bohl-27 <161889350+Bohl-27@users.noreply.github.com>
2025-04-02 09:29:53 -0400

    tank drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/DriveTrain/StandardTankDrive.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.RobotModel.DriveTrain;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Extensions.GamepadExtensions;
+
+public class OneStickTank extends DriveTrain
+{
+    // these can be declared Final because once they are initialized they should not be changed.
+    private final DcMotor left, right;
+
+    /**
+     * Initialize the Tank Drive with Configuration motors named:
+     * left -> "leftTread"
+     * right -> "rightTread"
+     * @param hardwareMap pass down from OpMode.
+     */
+    public TwoStickTank(HardwareMap hardwareMap)
+    {
+        left = hardwareMap.get(DcMotor.class, "leftTread");
+        right = hardwareMap.get(DcMotor.class, "rightTread");
+    }
+
+    @Override
+    public void drive(Gamepad gamepad)
+    {
+        float leftDrive = GamepadExtensions.GetLeftStickY(gamepad);
+        float rightDrive = GamepadExtensions.GetRightStickY(gamepad);
+        left.setPower(leftDrive);
+        right.setPower(rightDrive);
+    }
+
+}
