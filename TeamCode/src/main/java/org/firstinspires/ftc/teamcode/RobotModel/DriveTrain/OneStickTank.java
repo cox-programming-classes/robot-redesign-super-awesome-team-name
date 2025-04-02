d9a5f25b7e711d5122edd0f4e0362a75319ec9c6
Jason Cox <coxj@vt.edu>
2025-04-02 08:09:46 -0400

    Leroy Jenkins!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/DriveTrain/OneStickTank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotModel/DriveTrain/OneStickTank.java
@@ -3,14 +3,64 @@ package org.firstinspires.ftc.teamcode.RobotModel.DriveTrain;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Extensions.GamepadExtensions;
 
+
 public class OneStickTank extends DriveTrain
 {
+    private class LeroyState
+    {
+        ElapsedTime _stateTimer;
+        double _chargeTime;
+        boolean _isActive;
+
+        /**
+         * Instantiate a Leroy Jenkins state
+         * @param chargeTime duration of the blind charge in seconds
+         */
+        public LeroyState(double chargeTime)
+        {
+            _stateTimer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
+            _chargeTime = chargeTime;
+            _isActive = false;
+        }
+
+        /**
+         * LEROY JEEENKINS~!
+         */
+        public void activate()
+        {
+            left.setPower(1);
+            right.setPower(1);
+            _stateTimer.reset();
+            _isActive = true;
+        }
+
+        /**
+         * Are we still Charging?
+         * @return
+         */
+        public boolean isCompleted()
+        {
+            if(!_isActive)
+                return true;
+
+            double duration = _stateTimer.time();
+            if(duration < _chargeTime)
+                return false;
+
+            left.setPower(0);
+            right.setPower(0);
+            _isActive = false;
+            return true;
+        }
+    }
+
     // these can be declared Final because once they are initialized they should not be changed.
     private final DcMotor left, right;
-
+    private final LeroyState leroyState = new LeroyState(10);
     /**
      * Initialize the Tank Drive with Configuration motors named:
      * left -> "leftTread"
@@ -26,6 +76,16 @@ public class OneStickTank extends DriveTrain
     @Override
     public void drive(Gamepad gamepad)
     {
+        if(!leroyState.isCompleted())
+            return; // Leroy is in control!
+
+        if(gamepad.left_bumper && gamepad.right_bumper)
+        {
+            // LEROY JENKINS!
+            leroyState.activate();
+            return;
+        }
+
         float throttle = GamepadExtensions.GetLeftStickY(gamepad);
         float turnBias = GamepadExtensions.GetLeftStickX(gamepad);
         float extraTurnBias = GamepadExtensions.GetRightStickX(gamepad);
